9f6c7f692180bf286e0d6ac6dcca7d3f56c8dc7c
s21290 <s21290@pjwstk.edu.pl>
2023-03-15 23:05:49 +0100

    Changed view code in Employee Model

--- src/main/java/pl/coderstwo/employeemanager/model/Employee.java
+++ src/main/java/pl/coderstwo/employeemanager/model/Employee.java
@@ -10,11 +10,18 @@ public class Employee implements Serializable { //serializable pomaga w zmianie 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false, updatable = false)
     private Long id;
+
     private String name;
+
     private String email;
+
     private String jobTitle;
+
     private String phone;
+
     private String imageUrl;
+
     @Column(nullable = false, updatable = false)
     private String employeeCode;
+
 }
