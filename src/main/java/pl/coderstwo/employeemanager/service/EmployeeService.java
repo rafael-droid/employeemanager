2e5ac890cf33bb8ea6c5306d84d13f2f20f157f8
s21290 <s21290@pjwstk.edu.pl>
2023-03-16 17:20:37 +0100

    Added other Employee methods to EmployeeService

--- src/main/java/pl/coderstwo/employeemanager/service/EmployeeService.java
+++ src/main/java/pl/coderstwo/employeemanager/service/EmployeeService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import pl.coderstwo.employeemanager.model.Employee;
 import pl.coderstwo.employeemanager.repo.EmployeeRepo;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -19,5 +20,19 @@ public class EmployeeService {
     public Employee addEmployee(Employee employee){
         employee.setEmployeeCode(UUID.randomUUID().toString());
         return employeeRepo.save(employee);
+    }
+    public List<Employee> findAllEmployees(){
+        return employeeRepo.findAll();
+    }
+    public Employee updateEmployee(Employee employee){
+        return employeeRepo.save(employee);
+    }
+    public Employee findEmployeeById(Long id){
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(()->new UserNotFoundException ("User by id" +id+ "was not found"));
+    }
+
+    public void deleteEmployee(Long id){
+        employeeRepo.deleteEmployeeById(id);
     }
 }
