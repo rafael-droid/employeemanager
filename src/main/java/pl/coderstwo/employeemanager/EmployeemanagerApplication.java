7f24b39be58f04454bc3dd88b173ba77559d2115
s21290 <s21290@pjwstk.edu.pl>
2023-03-26 14:35:24 +0200

    Fixed connect backend and frontend

--- src/main/java/pl/coderstwo/employeemanager/EmployeemanagerApplication.java
+++ src/main/java/pl/coderstwo/employeemanager/EmployeemanagerApplication.java
@@ -18,19 +18,6 @@ public class EmployeemanagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeemanagerApplication.class, args);
 	}
-	@Bean
-	@SuppressWarnings("unchecked")
-	public FilterRegistrationBean simpleCorsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
-		config.setAllowedMethods(Collections.singletonList("*"));
-		config.setAllowedHeaders(Collections.singletonList("*"));
-		source.registerCorsConfiguration("/**", config);
-		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter());
-		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-		return bean;
-	}
+
 
 }
