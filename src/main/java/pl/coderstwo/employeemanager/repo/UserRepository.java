80a7da2968408c08762e05187841780d74c9b700
s21290 <s21290@pjwstk.edu.pl>
2023-04-01 00:33:21 +0200

    Added method findByUsername with check username if its in the db

--- src/main/java/pl/coderstwo/employeemanager/repo/UserRepository.java
+++ src/main/java/pl/coderstwo/employeemanager/repo/UserRepository.java
@@ -3,6 +3,9 @@ package pl.coderstwo.employeemanager.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.coderstwo.employeemanager.model.User;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByUsername(String username);
 
 }
--- src/main/java/pl/coderstwo/employeemanager/service/UserService.java
+++ src/main/java/pl/coderstwo/employeemanager/service/UserService.java
@@ -3,6 +3,7 @@ package pl.coderstwo.employeemanager.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
@@ -26,7 +27,13 @@ public class UserService {
     };
     @PostMapping("/")
     public ResponseEntity addUser(@RequestBody User user){
-        User savedUser = userRepository.save(user);
-        return ResponseEntity.ok(savedUser);
+        List<User> usersFromDB = userRepository.findByUsername(user.getUsername());
+
+        if(!usersFromDB.isEmpty()){
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+        }else {
+            User savedUser = userRepository.save(user);
+            return ResponseEntity.ok(savedUser);
+        }
     }
 }
